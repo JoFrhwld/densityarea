c2c474f6c36d551f8359311c5bdf22d37f68553d
JoFrhwld <JoFrhwld@gmail.com>
2023-09-15 14:49:24 -0400

    full name of booliean for r cmd check

--- tests/testthat/test-multi_polygon.R
+++ tests/testthat/test-multi_polygon.R
@@ -2,7 +2,7 @@ test_that("multi-modal densities work", {
   set.seed(10)
   x = c(rnorm(50), rnorm(50, mean = 3))
   y = c(rnorm(50), rnorm(50, mean = 3))
-  output = densityarea::density_polygons(x, y, probs = c(0.1), as_sf = T, as_list = F)
+  output = density_polygons(x, y, probs = c(0.1), as_sf = T, as_list = F)
   expect_s3_class(
     output$geometry,
     "sfc_MULTIPOLYGON"
@@ -14,7 +14,7 @@ test_that("multi-modal densities work", {
   x = rnorm(100)
   y = rnorm(100)
   probs = ppoints(10)
-  output = densityarea::density_polygons(x, y, probs = probs, as_sf = T, as_list = F)
+  output = density_polygons(x, y, probs = probs, as_sf = T, as_list = F)
   expect_equal(
     length(probs),
     nrow(output)
