f73cedaa1128f461acdb2076e45ff2dd43ad4cc0
JoFrhwld <JoFrhwld@gmail.com>
2023-09-28 14:47:20 -0400

    fixed tests for default returns

--- tests/testthat/test-list_return.R
+++ tests/testthat/test-list_return.R
@@ -1,39 +1,39 @@
-test_that("`density_polygons()` returns list by default", {
+test_that("`density_polygons()` returns a list data frame when asked", {
+  set.seed(10)
+  x <- rnorm(100)
+  y <- rnorm(100)
+  output <- densityarea::density_polygons(x = x, y = y, as_list = T)
+  expect_type(output,"list")
+  expect_s3_class(output, NA)
+  expect_s3_class(output[[1]], "data.frame")
+})
+
+
+test_that("`density_polygon()` returns data frame by default", {
   set.seed(10)
   x <- rnorm(100)
   y <- rnorm(100)
   output <- densityarea::density_polygons(x = x, y = y)
-  expect_type(output,"list")
-  expect_s3_class(output, NA)
-  expect_s3_class(output[[1]], "data.frame")
-})
-
-
-test_that("`density_polygon()` returns dataframe when asked", {
-  set.seed(10)
-  x <- rnorm(100)
-  y <- rnorm(100)
-  output <- densityarea::density_polygons(x = x, y = y, as_list = F)
   expect_s3_class(output,"data.frame")
 })
 
 
-test_that("`density_area()` returns list by default", {
+test_that("`density_area()` returns list by when asked", {
   set.seed(10)
   x <- rnorm(100)
   y <- rnorm(100)
-  output <-   densityarea::density_area(x = x, y = y)
+  output <-   densityarea::density_area(x = x, y = y, as_list = T)
   expect_type(output,"list")
   expect_s3_class(output, NA)
   expect_s3_class(output[[1]], "data.frame")
 })
 
 
-test_that("`density_area()` returns a dataframe when asked", {
+test_that("`density_area()` returns a data frame by default", {
   set.seed(10)
   x <- rnorm(100)
   y <- rnorm(100)
-  output <-   densityarea::density_area(x = x, y = y, as_list = F)
+  output <-   densityarea::density_area(x = x, y = y)
   expect_s3_class(output, "data.frame")
 })
 
